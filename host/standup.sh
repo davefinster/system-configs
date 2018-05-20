ef67ce34064dc21d5959c0ba6cb0d2356fa9f429
Dave Finster <davefinster@me.com>
2018-05-20 13:38:39 -0700

    Better moving of the MOTD

--- host/standup.sh
+++ host/standup.sh
@@ -117,7 +117,8 @@ fi
 if [ ! -d "/etc/update-motd.d.bak" ]; then
     sudo mv /etc/update-motd.d /etc/update-motd.d.bak
 fi
-sudo cp -r ./motd /etc/update-motd.d
+sudo mkdir /etc/update-motd.d
+sudo cp -r ./motd/* /etc/update-motd.d
 sudo chown -R root /etc/update-motd.d
 sudo chgrp -R root /etc/update-motd.d
 
